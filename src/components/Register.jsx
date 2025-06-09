07110cd18a0c7f4143089388cae7e3d7cc5e2153
para677ruk <epicgamesfalloutnew@gmail.com>
2025-06-09 23:59:39 +0400

    Update Register.jsx

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -10,7 +10,7 @@ const Register = () => {
 
   const onSubmit = async (data) => {
     try {
-      const res = await fetch('http://localhost:5000/api/auth/register', {
+      const res = await fetch('http://https://grstore-backend.onrender.com/api/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(data)
